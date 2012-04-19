0f7388e4687a6b6089b779d0b87497c035a6fb79
paweda home <pargles@jupter.(none)>
2012-04-19 20:49:18 -0300

    alterado o Main e removido o teste.java

--- Main.java
+++ Main.java
@@ -3,19 +3,25 @@ import java.util.Arrays;
 
 import javax.swing.JFrame;
 
-/*
- * Trabalho para a disciplina de Inteligência Artificial - prof.: Anderson Ferrugem
- * 		Alunos: Abilio Gambim Parada
- * 				Vanderson Oliveira da Silva
+/**
+ *
+ * @author abilio and pargles
+ * @version 1.0
  */
-public class Main {
+public class Main 
+{
 	
-	public static void main(String[] args) throws IOException {
-		JFrame window = new JFrame("N-Rainhas IA");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setContentPane(new NrainhasUI());
-        window.pack();  
-        window.show();  
-        window.setResizable(false);
+	public static void main(String[] args) throws IOException 
+	{
+          	NQueens programa = new NQueens();
+          	programa.executa();
+          	/*
+          	JFrame window = new JFrame("N-Rainhas IA");
+          	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          	window.setContentPane(new NrainhasUI());
+          	window.pack();
+          	window.show();
+          	window.setResizable(false);
+          	*/
 	}
 }
