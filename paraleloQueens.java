53ba13af865c95bb4acb0b2bf85813b8026f385f
Abilio Gambim Parada <abiliogp@positivo.(none)>
2012-04-19 18:12:55 -0300

    modificação no método putBoard

--- paraleloQueens.java
+++ paraleloQueens.java
@@ -48,7 +48,7 @@ public class paraleloQueens extends Thread {
 		arquivo = new FileWriter("out.txt",true);// true é para nao
 													// sobrescrever quando vai
 													// escrever algo
-		//synchronized(arquivo){
+		synchronized(this){
 			BufferedWriter bw = new BufferedWriter(arquivo);
 			System.out.println("\n\nSolution " + (++s));
 			System.out.print("Vetor: " + solution);
@@ -56,7 +56,7 @@ public class paraleloQueens extends Thread {
 			System.out.println();
 			bw.newLine();
 			bw.close();
-		//}
+		}
 	}
 
 	@Override
